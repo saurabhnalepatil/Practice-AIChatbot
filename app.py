3380b1792b1d23ed9232db12630e8cc92410cf02
saurabhnalepatil <saurabhnalepatil@gmail.com>
2024-11-20 11:11:18 +0530

    Updated the requirements.txt file.

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ from langchain.tools.render import format_tool_to_openai_function
 from langchain_core.messages import BaseMessage, FunctionMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import Field
-from langchain_core.utils.function_calling import convert_to_openai_tool
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END, StateGraph
 from langgraph.prebuilt import ToolExecutor, ToolInvocation
